029418b517e9359de22f7b3a2ef3fa26a34ea40d
raffaelebaiocco06-ux <raffaele.baiocco06@gmail.com>
2026-04-27 11:01:36 +0200

    starter

---
+++ build-week/src/main/java/Build_week/build_week/BuildWeekApplication.java
@@ -0,0 +1,13 @@
+package Build_week.build_week;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildWeekApplication.class, args);
+	}
+
+}
---
+++ build-week/src/test/java/Build_week/build_week/BuildWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package Build_week.build_week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
