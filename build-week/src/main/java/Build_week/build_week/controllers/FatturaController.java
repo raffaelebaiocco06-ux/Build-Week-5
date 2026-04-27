3c732fb4cbd9b22a306b543df1780d39f651625e
Fabrizio Pesaresi <fabryzio2391@gmail.com>
2026-04-27 16:05:46 +0200

    Struttura base modulo fatture

---
+++ build-week/src/main/java/Build_week/build_week/controllers/FatturaController.java
@@ -0,0 +1,71 @@
+package Build_week.build_week.controllers;
+
+import Build_week.build_week.entities.Fattura;
+import Build_week.build_week.services.FatturaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/fatture")
+public class FatturaController {
+
+    @Autowired
+    private FatturaService fatturaService;
+
+    @GetMapping
+    public Page<Fattura> getAll(Pageable pageable) {
+        return fatturaService.findAll(pageable);
+    }
+
+    @GetMapping("/{id}")
+    public Fattura getById(@PathVariable UUID id) {
+        return fatturaService.findById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public Fattura create(@RequestBody Fattura body) {
+        return fatturaService.save(body);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public void delete(@PathVariable UUID id) {
+        fatturaService.delete(id);
+    }
+
+    @GetMapping("/filter-cliente/{clienteId}")
+    public Page<Fattura> getByCliente(@PathVariable UUID clienteId, Pageable pageable) {
+        return fatturaService.findByCliente(clienteId, pageable);
+    }
+
+    @GetMapping("/filter-stato")
+    public Page<Fattura> getByStato(@RequestParam String stato, Pageable pageable) {
+        return fatturaService.findByStato(stato, pageable);
+    }
+
+    @GetMapping("/filter-data")
+    public Page<Fattura> getByData(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate data, Pageable pageable) {
+        return fatturaService.findByData(data, pageable);
+    }
+
+    @GetMapping("/filter-anno")
+    public Page<Fattura> getByAnno(@RequestParam int anno, Pageable pageable) {
+        return fatturaService.findByAnno(anno, pageable);
+    }
+
+    @GetMapping("/filter-importo")
+    public Page<Fattura> getByImporto(@RequestParam Double min, @RequestParam Double max, Pageable pageable) {
+        return fatturaService.findByRangeImporto(min, max, pageable);
+    }
+}
---
+++ build-week/src/main/java/Build_week/build_week/services/FatturaService.java
@@ -0,0 +1,55 @@
+package Build_week.build_week.services;
+
+import Build_week.build_week.entities.Fattura;
+import Build_week.build_week.repositories.FatturaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Service
+public class FatturaService {
+
+    @Autowired
+    private FatturaRepository fatturaRepository;
+
+    public Fattura save(Fattura fattura) {
+        return fatturaRepository.save(fattura);
+    }
+
+    public Page<Fattura> findAll(Pageable pageable) {
+        return fatturaRepository.findAll(pageable);
+    }
+
+    public Fattura findById(UUID id) {
+        return fatturaRepository.findById(id).orElseThrow(() -> new RuntimeException("Fattura non trovata"));
+    }
+
+    public void delete(UUID id) {
+        Fattura f = this.findById(id);
+        fatturaRepository.delete(f);
+    }
+
+    public Page<Fattura> findByCliente(UUID clienteId, Pageable pageable) {
+        return fatturaRepository.findByClienteId(clienteId, pageable);
+    }
+
+    public Page<Fattura> findByStato(String stato, Pageable pageable) {
+        return fatturaRepository.findByStatoIgnoreCase(stato, pageable);
+    }
+
+    public Page<Fattura> findByData(LocalDate data, Pageable pageable) {
+        return fatturaRepository.findByData(data, pageable);
+    }
+
+    public Page<Fattura> findByAnno(int anno, Pageable pageable) {
+        return fatturaRepository.findByAnno(anno, pageable);
+    }
+
+    public Page<Fattura> findByRangeImporto(Double min, Double max, Pageable pageable) {
+        return fatturaRepository.findByImportoBetween(min, max, pageable);
+    }
+}
