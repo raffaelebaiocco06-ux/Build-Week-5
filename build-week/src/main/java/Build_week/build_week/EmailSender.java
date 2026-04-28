a128f08d371fc1b3d2e4cb0641e0464b0975ee3a
cicalecristian <cicalecristian@gmail.com>
2026-04-28 13:08:25 +0200

    EmailSender created

---
+++ build-week/src/main/java/Build_week/build_week/EmailSender.java
@@ -0,0 +1,31 @@
+package Build_week.build_week;
+
+import Build_week.build_week.entities.Utente;
+import kong.unirest.core.HttpResponse;
+import kong.unirest.core.JsonNode;
+import kong.unirest.core.Unirest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailSender {
+    private final String domainName;
+    private final String apiKey;
+
+    public EmailSender(@Value("${mailgun.domainName}") String domainName, @Value("${mailgun.apiKey}") String apiKey) {
+        this.domainName = domainName;
+        this.apiKey = apiKey;
+    }
+
+    public void sendRegistrationEmail(Utente recipient) {
+        HttpResponse<JsonNode> response = Unirest.post("https://api.mailgun.net/v3/" + this.domainName + "/messages")
+                .basicAuth("api", this.apiKey)
+                .queryString("from", "cicalecristian@gmail.com")
+                .queryString("to", recipient.getEmail())
+                .queryString("subject", "Benvenuto sulla nostra piattaforma!")
+                .queryString("text", "Ciao " + recipient.getNome() + ", la tua registrazione è andata a buon fine!")
+                .asJson();
+
+        System.out.println(response.getBody());
+    }
+}
