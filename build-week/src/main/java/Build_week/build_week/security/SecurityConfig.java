ecdce4d6aeb25349f271eb8c3c37e1b9c79e56c5
raffaelebaiocco06-ux <raffaele.baiocco06@gmail.com>
2026-04-30 12:42:49 +0200

    5174

--- build-week/src/main/java/Build_week/build_week/security/SecurityConfig.java
+++ build-week/src/main/java/Build_week/build_week/security/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173/"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5174/"));
 
 
         //per ora ho fatto che permette tutti i metodi get post ecc.. se si vuole cambiare si puo scrivere sotto
